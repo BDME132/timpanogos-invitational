81f5ff0a4cec228cd02cc29ba592bfd10090a1fe
ben meilstrup <benmeilstrup@gmail.com>
2026-06-15 16:25:03 -0600

    Add sitemap integration to Astro configuration
    - Updated `astro.config.mjs` to include `@astrojs/sitemap` integration.
    - Added `@astrojs/sitemap` as a dependency in `package.json`.
    - Updated `pnpm-lock.yaml` to reflect the new dependency and its version.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.timpinvite.com',
   trailingSlash: 'ignore',
   output: 'static',
+  integrations: [sitemap()],
   build: {
     assets: 'assets',
   },
